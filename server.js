69e51161a20b1b525c25e504331e69a457dd6cd3
idreesalmasri <idrees.massri95@gmail.com>
2022-04-14 01:02:17 +0300

    test

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
-
+// var fs= require("fs");
 const app = express()
+app.use(express.static('./'));
+app.use(express.static(__dirname + '/views'));
 const bcrypt = require('bcrypt')
 const { users } = require('./models/index.js');
 const authentication = require('./middlewares/baicAuth.js');
 const bearerAuth = require('./middlewares/bearerAuth');
 
-
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+
 app.get("/" ,(req, res) => {
-    res.render('index.ejs');
+    // res.writeHead(200,{"Content-Type":"text/html"});
+    // fs.readFile("./index.html",null,function(error,data){
+    //     if(error){
+    //         res.writeHead(404);
+    //         res.writeHead("not found")
+    //     }else{
+    //         res.write(data);
+    //     }
+    //     res.end();
+    // })
+    // res.render('index.ejs');
+    res.render('index.html');
+    // res.setHeader('Content-Type', 'text/html');
+    // fs.createReadStream("index.html").pipe(res);
+
+    // res.sendFile(__dirname + "/index.html");
 })
 app.get("/login", (req, res) => {
     res.render('login.ejs');
@@ -33,14 +51,16 @@ app.post("/register", async (req, res) => {
     // res.render('register.ejs');
 })
 app.post('/login',authentication,(req,res)=>{
-    res.render('index.ejs');
+    
+    res.sendFile(__dirname + "/index.html");
+    
 })
 app.get("/register", (req, res) => {
     res.render('register.ejs');
 })
 
-app.get("/secret",bearerAuth,async (req, res) => {
-    res.status(200).json(req.user);
+app.get("/support",bearerAuth,async (req, res) => {
+    res.sendFile(__dirname + "/chat.html");
 })
 function start(port) {
     app.listen(port,()=>{
@@ -51,4 +71,3 @@ module.exports = {
     app: app,
     start: start
 }
-// app.listen(3000);
